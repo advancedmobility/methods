72b102d75daee5086b6c640bdbe381ab27a38d39
Minghao Zhu <zhum@trcpg.com>
2025-07-02 13:54:53 -0400

    feat: add lane and sign evaluation pipelin

---
+++ NN_models/sign_detection/code/docker/source_env.sh
@@ -0,0 +1,3 @@
+export AMSIGN_DATA="/media/dev/TRC_X9/I-70/NN_models/sign_detection/data"
+export AMSIGN_CODE="/media/dev/TRC_X9/I-70/NN_models/sign_detection/code"
+export AMSIGN_ARGS="--workdir /project/amsign_model --imgdir /data/event_images"
